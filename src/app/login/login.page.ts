b9815450fe71a0487753e016f67f4f8bb556ae9b
patrickmurray73 <g00344530@gmit.ie>
2020-04-16 13:44:57 +0100

    Added saveForm() to signup page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/Storage';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,7 @@ export class LoginPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Storage } from '@ionic/storage';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  ionicForm: FormGroup;
+  defaultDate = "1987-06-30";
+  isSubmitted = false;
+  users = [];
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder, private storage: Storage, private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.printDatabase();
+    this.ionicForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      dob: [this.defaultDate],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
   }
 
+  getDate(e) {
+    let date = new Date(e.target.value).toISOString().substring(0, 10);
+    this.ionicForm.get('dob').setValue(date, {
+       onlyself: true
+    })
+  }
+
+  get errorControl() {
+  return this.ionicForm.controls;
+  }
+
+submitForm() {
+  this.isSubmitted = true;
+  if (!this.ionicForm.valid) {
+    console.log('Please provide all the required values!')
+    return false;
+  } else {
+    this.saveForm();
+  }
+ }
+
+ saveForm() {
+  this.storage.get("user")
+      .then((data) => {
+        console.log(data);
+          this.users = data;
+          console.log(this.users);
+          console.log(this.ionicForm.value);
+          this.users.push(this.ionicForm.value);
+          console.log(this.users);
+          this.storage.set("user", this.users)
+            .then(
+              ()=> {
+                this.printDatabase();
+              })
+              .catch();
+        })
+      .catch();
+ }
+
+  printDatabase()
+  {
+    this.storage.get("user")
+      .then((data) => {
+          console.log(data);
+        })
+      .catch();
+  }
 }
