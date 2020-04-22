cebff9c9295b2358cf6bdf73929db11056bf6418
patrickmurray73 <g00344530@gmit.ie>
2020-04-22 16:07:04 +0100

    Comments added to signup.ts

--- src/app/Services/btc-service.service.ts
+++ src/app/Services/btc-service.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class BtcService {
 
   constructor(private http:HttpClient) { }
   
+  /*
+    This function fetches the data for the bitcoin/dollar chart from an api
+  */
   GetBtcData():Observable<any> {
     return this.http.get('https://api.gemini.com/v2/candles/btcusd/1m');
    }
--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -9,17 +9,21 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-  ionicForm: FormGroup;
-  defaultDate = "1987-06-30";
-  isSubmitted = false;
-  users = [];
-  accountNum: any;
+  // Declare variables
+  signupForm:FormGroup;
+  defaultDate:string = "1987-06-30";
+  isSubmitted:boolean = false;
+  users:any = [];
+  accountNum: number;
 
   constructor(public formBuilder: FormBuilder, private storage: Storage, private navCtrl: NavController) { }
 
-  ngOnInit() {
-    this.printDatabase();
-    this.ionicForm = this.formBuilder.group({
+  /*
+    When the signup page loads, the signupForm is initialised with validators.
+    The next account number is also generated.
+  */
+  ngOnInit():void {
+    this.signupForm = this.formBuilder.group({
       accountNum: [''],
       dollar: [''],
       name: ['', [Validators.required, Validators.minLength(2)]],
@@ -30,77 +34,81 @@ export class SignupPage implements OnInit {
     this.getAccountNum();
   }
 
-  getDate(e) {
+  /* 
+    This function is called once the date of birth has been changed.
+    It creates the date that was chosen and sets the corresponding dob value
+    in the signup form.
+  */
+  getDate(e):void {
     let date = new Date(e.target.value).toISOString().substring(0, 10);
-    this.ionicForm.get('dob').setValue(date, {
+    this.signupForm.get('dob').setValue(date, {
        onlyself: true
     })
   }
 
-  get errorControl() {
-  return this.ionicForm.controls;
+  /*
+    Returns signupForm control errors.
+  */
+  get errorControl():any {
+    return this.signupForm.controls;
   }
 
-submitForm(value: any) {
-  this.isSubmitted = true;
-  if (!this.ionicForm.valid) {
-    console.log('Please provide all the required values!')
-    return false;
-  } else {
-    value.dollar = 50000;
-    value.accountNum = this.accountNum;
-    this.saveForm();
+/*
+    Called once Sign Up button is clicked, if the form is
+    invalid a message will be displayed. If the form is valid
+    we give the user 50000 dollars and assign an accountNum
+    to the user. The saveForm() function is then called.
+  */
+  submitForm(value: any):boolean {
+    this.isSubmitted = true;
+    if (!this.signupForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      value.dollar = 50000;
+      value.accountNum = this.accountNum;
+      this.saveForm();
+    }
   }
- }
 
- saveForm() {
-  this.storage.get("user")
+ /*
+  This function saves the validated form to localStorage,
+  creating a new user.
+ */
+ saveForm():void {
+  this.storage.get("user") 
       .then((data) => {
-        if(data != null) {
+        if(data != null) { // Add to end of users
           this.users = data;
-          this.users.push(this.ionicForm.value);
-        }
-        else {
-          this.users[0] = this.ionicForm.value;
+          this.users.push(this.signupForm.value);
+        } else { // First user
+          this.users[0] = this.signupForm.value;
         }
           this.storage.set("user", this.users)
-            .then(
-              ()=> {
-                this.printDatabase();
-              })
-              .catch();
-        })
-      .catch();
- }
-
-  printDatabase()
-  {
-    this.storage.get("user")
-      .then((data) => {
-          if(data == null)
-          {
-            console.log("Database Empty!")
-          }
-          else {
-            console.log(data);
-          }
+           .then()
+           .catch();
         })
       .catch();
   }
 
-  getAccountNum() {
+  /*
+    This function generates a new unique account number.
+    It does so by getting accountNum in storage, if it doesnt
+    exist then it creates the current accountNum as 1. If an
+    accountNum exists, the function increments it and saves it in
+    storage.
+  */
+  getAccountNum():void {
     this.storage.get("accountNum")
     .then((data) => {
-      if(data == null)
-      {
+      if(data == null) { // AccountNum doesnt exist
         this.storage.set("accountNum", 1)
         .then(() => {
           console.log("No numbers yet");
           this.accountNum = 1;
         })
         .catch();
-      }
-      else {
+      } else { // Increment accountNum and save it
         this.accountNum = data + 1;
         this.storage.set("accountNum", this.accountNum)
         .then(() => {
