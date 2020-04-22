60a9b27de4e12f950b4f3e7713b8edbba39e5e4e
patrickmurray73 <g00344530@gmit.ie>
2020-04-22 16:19:10 +0100

    Comments added to account page

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -21,6 +21,11 @@ export class AccountPage implements OnInit {
 
   constructor(public storage: Storage) { }
 
+  /**
+   * When account page is called, we get the current logged
+   * in user and populate our variables to display the current
+   * account information to the user.
+   */
   ngOnInit() {
    this.storage.get("loggedIn")
       .then((data) => {
@@ -36,18 +41,38 @@ export class AccountPage implements OnInit {
       .catch();
   }
 
+  /**
+   * This function is called once the date of birth has been changed.
+   * It creates the date that was chosen and sets the value of the dob
+   * variable
+   * 
+   * @param e 
+   */
   getDate(e) {
     let date = new Date(e.target.value).toISOString().substring(0, 10);
     this.dob = date;
     console.log(this.dob);
   }
 
+  /**
+   * When user clickes the edit button, this function is called.
+   * When called, it allows the user to edit the account details, while
+   * disabling the edit button and enabling the save changes button.
+   */
   edit() {
     this.isReadOnly = false;
     this.editDisabled = true;
     this.saveDisabled = false;
   }
 
+  /**
+   * Called when user clicks the save changes button.
+   * This function the user variables are set and the details
+   * of the current logged in user in local storage is updated.
+   * The account number of this user is then searched in local 
+   * storage and once found, their details are also changed to 
+   * that of the current logged in user.
+   */
   saveChanges() {
     this.user.name = this.name;
     this.user.email = this.email;
@@ -59,16 +84,11 @@ export class AccountPage implements OnInit {
         this.storage.get("user")
         .then((data) => {
           let users: any = data;
-          for(let i = 0; i < users.length; i++)
-          {
-            if(users[i].accountNum == this.accountNum)
-            {
+          for(let i = 0; i < users.length; i++) {
+            if(users[i].accountNum == this.accountNum) {
               users[i] = this.user;
-              console.log(this.user);
               this.storage.set("user", users)
-              .then(() => {
-                console.log(users[i]);
-              })
+              .then()
               .catch();
             }
           }
@@ -76,11 +96,18 @@ export class AccountPage implements OnInit {
         .catch();
         })
       .catch();
+      // Make details read only, enable edit button and disable
+      // save changes button.
       this.isReadOnly = true;
       this.editDisabled = false;
       this.saveDisabled = true;
   }
 
+  /**
+   * This function is called when the reset account button is clicked.
+   * It sets the balance of the user to 50000, the default balance and calls
+   * the saveChanges() function.
+   */
   resetAccount() {
     this.balance = 50000;
     this.saveChanges();
